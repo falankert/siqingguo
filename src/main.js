a7de6d292a973d99076b4a0bca2bc50244c9b3d5
falan <falankert@gmail.com>
2025-08-30 05:58:11 +0900

    feat: add minimal Vite template for GitHub Pages

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 ﻿document.querySelector("#app").innerHTML = `
   <div class="wrap">
     <h1>四清果 · Vite on GitHub Pages</h1>
-    <p>部署成功！现在你可以开始开发啦。</p>
+    <p>🚀 部署成功！现在你可以愉快地开发啦。</p>
   </div>
 `;
--- vite.config.ts
+++ vite.config.ts
@@ -1,2 +1,5 @@
-﻿import { defineConfig } from 'vite';
-export default defineConfig({ base: '/siqingguo/' });
+﻿import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/siqingguo/",
+});
