11ebd0679e268035d878727c2a0791911bb46c1a
falan <falankert@gmail.com>
2025-08-30 04:29:51 +0900

    fix: set base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
-﻿import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-export default defineConfig({ plugins: [react()], base: '/siqingguo/' });
+﻿import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/siqingguo/",
+});
