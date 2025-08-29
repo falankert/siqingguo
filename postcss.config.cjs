895339efeef1473e52257e7c643f658c0fc6d209
falan <falankert@gmail.com>
2025-08-30 05:31:11 +0900

    fix: replace broken PostCSS JSON with minimal JS config; reinstall deps

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,1 +1,9 @@
-﻿module.exports={plugins:{tailwindcss:{},autoprefixer:{}}};
+﻿/** Minimal PostCSS config to satisfy Vite build */
+module.exports = {
+  plugins: {
+    /* 可以按需启用插件，例如：
+    // 'postcss-nesting': {},
+    // autoprefixer: {}
+    */
+  }
+};
